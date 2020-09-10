4133c9f5ef98bb2cbc8f37b2aa5a17819ad5fb67
gavinev98 <gavin.everett48@hotmail.com>
2020-09-10 15:02:37 +0100

    Added function to convert object to summary.

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,6 +1,29 @@
 import React from 'react';
 
+import Aux from '../../../hoc/Aux';
+
 const orderSummary = (props) => {
+    //taking in the ingredients in via props and using object.keys to etract string values.
+    const ingredientSummary = Object.keys(props.ingredientSummary)
+                                .map(igKey => {
+                                    return <li><span>{igKey}</span>: {props.ingredients[igKey]}</li>
+                                });
+
+
+    return (
+        <Aux>
+        <h3>Your Order</h3>
+        <p>A delicious buger with the following ingredients</p>
+
+        <ul>
+       
+
+
+        </ul>
+
+
+        </Aux>
+    );
 
 };
 
