85fbdeef19f8f18e766ec8b0f7428ea5280cf7ba
gavinev98 <gavin.everett48@hotmail.com>
2020-09-10 15:53:45 +0100

    added backdrop ct to modal component.

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -2,10 +2,15 @@ import React from 'react'
 
 import classes from './Modal.module.css'
 
+import Aux from '../../../hoc/Aux';
+
+import Backdrop from '../Backdrop/Backdrop';
+
 const modal = (props) => (
 
+    <Aux>
+        <Backdrop clicked={props.modalClosed} show={props.show} />
     <div
-     
      className={classes.Modal}
      style={{
          transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
@@ -14,6 +19,7 @@ const modal = (props) => (
      >
         {props.children}
     </div>
+    </Aux>
 
 
 
