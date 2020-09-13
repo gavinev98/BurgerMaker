a4073dee1a6dd41fdddc8425ebf362cdcd1a1b82
gavinev98 <gavin.everett48@hotmail.com>
2020-09-13 15:54:05 +0100

    added css to toolbar

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -15,7 +15,7 @@ const toolbar = (props) => (
      <div className={classes.Logo}>
      <Logo />
      </div>
-     <nav>
+     <nav className={classes.DesktopOnly}>
      <NavigationItems />
      </nav>
     </header>
