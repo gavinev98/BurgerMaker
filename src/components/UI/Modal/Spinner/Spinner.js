04714514d9f7969f6536e2f253eb2fc7b0607a49
gavinev98 <gavin.everett48@hotmail.com>
2020-09-17 16:28:07 +0100

    added spinner class

---
+++ src/components/UI/Modal/Spinner/Spinner.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import classes from '../Spinner/Spinner.module.css'
+
+const spinner = () => {
+<div className={classes.Loader}>Loading...</div>
+
+}
+
+export default spinner;
