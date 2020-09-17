b113a4f345305e364f5c03255c52d807a214aa31
gavinev98 <gavin.everett48@hotmail.com>
2020-09-17 17:20:53 +0100

    added spinner functional component to order sum.

--- src/components/UI/Modal/Spinner/Spinner.js
+++ src/components/UI/Modal/Spinner/Spinner.js
@@ -2,9 +2,10 @@ import React from 'react';
 
 import classes from '../Spinner/Spinner.module.css'
 
-const spinner = () => {
+const spinner = () => ( 
+
 <div className={classes.Loader}>Loading...</div>
 
-}
+);
 
 export default spinner;
