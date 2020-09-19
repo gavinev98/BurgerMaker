6a2cfd71ce4bccc3289028ed1bb767139b5774fc
gavinev98 <gavin.everett48@hotmail.com>
2020-09-19 19:12:58 +0100

    ejecting interceptors to prevent memory leaks.

--- src/withErrorHandler/withErrorHandler.js
+++ src/withErrorHandler/withErrorHandler.js
@@ -4,6 +4,8 @@ import Modal from '../components/UI/Modal/Modal';
 
 import Aux from '../hoc/Aux';
 
+import axios from 'axios';
+
 
 const withErrorHandler = (WrappedComponent, axios) => {
 
@@ -14,19 +16,31 @@ const withErrorHandler = (WrappedComponent, axios) => {
             }
 
 
+
+            //lifecycle methods.
             //checking status recieved from axios using componentdidmount
             componentWillMount() {
                 //using interceptors to check request and response at endpoints.
                 //setting error to null for each request sent.
-                axios.interceptors.request.use(req => {
+                this.requestInterceptor = axios.interceptors.request.use(req => {
                     this.setState({error : null});
                     return req;
                 })
 
                 //setting error to error for each response.
-                axios.interceptors.response.use(res => res, error => {
+                this.responseInterceptor = axios.interceptors.response.use(res => res, error => {
                     this.setState({ error: error});
                 })
+            }
+
+            //unmounting the  component when it is not needed anymore / prevents memory leaks.
+            //remove interceptors.
+            componentWillUnmount () {
+                //ejecting interceptors once not need anymore.
+                console.log('Will Unmount', this.requestInterceptor, this.responseInterceptor);
+                axios.interceptors.request.eject(this.requestInterceptor);
+                axios.interceptors.response.eject(this.responseInterceptor);
+
             }
 
             errorConfirmedHandler = () => {
