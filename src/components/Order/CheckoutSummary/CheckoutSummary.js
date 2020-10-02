ae6adb691ea6f33c9fdffe1277cb3ef49c8fb58c
gavinev98 <gavin.everett48@hotmail.com>
2020-10-02 11:55:46 +0100

    adding routing to burger project.

--- src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -2,18 +2,20 @@ import React from 'react';
 
 import Burger from '../../Burger/Burger'
 
-import Button from '../../UI/Button';
+import Button from '../../UI/Button/Button';
+
+import classes from './CheckoutSummary.module.css';
 
 const checkoutSummary = (props) => {
 
     return (
-        <div>
+        <div className={classes.checkoutSummary}>
             <h1>We hope it tastes well!</h1>
-            <div style={{width: '300px', height: '300px', margin: 'auto'}}>
+            <div style={{width: '100%', margin: 'auto'}}>
                 <Burger ingredients={props.ingredients} />
             </div>
-            <Button btnType="Danger" clicked={}>CANCEL</Button>
-            <Button btnType="Success" clicked={}>CONTINUE</Button>
+            <Button btnType="Danger" clicked="">CANCEL</Button>
+            <Button btnType="Success" clicked="">CONTINUE</Button>
 
         </div>
     )
--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,7 +1,31 @@
 import React, { Component } from 'react';
 
+import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
+
 class Checkout extends Component {
 
+
+    state = {
+        ingredients: {
+            salad: 1,
+            meat: 1,
+            cheese: 1,
+            bacon: 1
+        }
+    }
+
+
+    //goal is to create a checkout summary show burger,
+    render() {
+        return (
+            <div>
+                <CheckoutSummary ingredients={this.state.ingredients} />
+
+
+            </div>
+
+        );
+    }
 
 
 }
