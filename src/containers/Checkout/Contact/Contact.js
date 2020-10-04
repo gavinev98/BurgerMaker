9de5d95f53414288518e6e6f7b24f37bccb072ec
gavinev98 <gavin.everett48@hotmail.com>
2020-10-04 10:46:39 +0100

    setup contact component sub of checkout.

---
+++ src/containers/Checkout/Contact/Contact.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+
+class Contact extends Component {
+
+    state = {
+        name : '',
+        email: '',
+        address: {
+            street: '',
+            postalCode: ''
+        }
+    }
+
+
+}
+
+
+export default Contact;
