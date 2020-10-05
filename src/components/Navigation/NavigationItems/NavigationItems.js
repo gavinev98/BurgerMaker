fd1c2755593ebe6a1f2358dfc530fc361597f64a
gavinev98 <gavin.everett48@hotmail.com>
2020-10-05 11:02:01 +0100

    updating links to NavLinks for react-router-dom

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -8,8 +8,8 @@ import NavigationItem from './NavigationItem';
 const navigationItems = () => (
 
         <ul className={classes.NavigationItems}>
-        <NavigationItem link="/" active> Burger Builder </NavigationItem>
-        <NavigationItem link="/" > Check </NavigationItem>
+        <NavigationItem link="/"> Burger Builder </NavigationItem>
+        <NavigationItem link="/orders" > Orders </NavigationItem>
         </ul>
 
 );
