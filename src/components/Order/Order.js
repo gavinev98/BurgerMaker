7019fbf7602402bc003c8cf6956fd116858a1b72
gavinev98 <gavin.everett48@hotmail.com>
2020-10-05 15:00:40 +0100

    loop over ingredients object array to array.

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -2,13 +2,24 @@ import React from 'react';
 
 import classes from './Order.module.css';
 
-const order = (props) => (
+const order = (props) => {
 
+    const ingredients = [];
+
+    for(let ingredientName in props.ingredients){
+        ingredients.push({name: ingredientName,
+                         amount : props.ingredients[ingredientName]})
+    }
+
+
+    return(        
     <div className={classes.Order}>
-        <p>Ingredients : Salad  (1) </p>
-        <p>Price : <strong>USD 5.45</strong> </p>
+    <p>Ingredients : Salad  (1) </p>
+    <p>Price : <strong>USD {props.price}</strong> </p>
     </div>
+    );
 
-);
+
+};
 
 export default order;
