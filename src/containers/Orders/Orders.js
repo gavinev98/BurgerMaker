fa5e2d73c11d24be5d51f3d1cab7adafb5a3b3f3
gavinev98 <gavin.everett48@hotmail.com>
2020-10-05 10:36:17 +0100

    added skeleton for orders class

---
+++ src/containers/Orders/Orders.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+
+
+class Orders extends Component {
+
+
+    state = {
+
+
+    }
+
+
+
+    render() {
+
+        return(
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default Orders;
