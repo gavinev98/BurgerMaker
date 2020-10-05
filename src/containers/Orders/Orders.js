22a37676920954fa0c4aa348d7c131ba937a22ab
gavinev98 <gavin.everett48@hotmail.com>
2020-10-05 14:53:16 +0100

    added price to each order.

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -49,7 +49,11 @@ class Orders extends Component {
         return(
             <div>
                 {this.state.orders.map(order => (
-                    <Order key={order.id} />
+                    <Order 
+                    key={order.id}
+                    ingredients={order.ingredients}
+                    price={order.price}
+                    />
                 ))}
             </div>
         );
