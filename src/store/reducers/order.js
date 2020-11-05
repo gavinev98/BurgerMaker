8c53ffc0270ae43dd745824efe4d64ce51b76fdc
gavinev98 <gavin.everett48@hotmail.com>
2020-11-05 17:09:07 +0000

    copying array.

--- src/store/reducers/order.js
+++ src/store/reducers/order.js
@@ -16,10 +16,21 @@ const reducer = (state = initialState, action) => {
         switch (action.type) {
 
             case actionTypes.PURCHASE_BURGER_SUCCESS:
-                return {};
+                const newOrder = {
+                    ...action.orderData,
+                    id: action.
+                }
+                return {
+                    ...state,
+                    loading: false,
+                    orders: state.orders.concat()
+
+                };
             case actionTypes.PURCHASE_BURGER_FAIL:
                 return {};
             default:
                 return state;
         }
 }
+
+export default reducer;
