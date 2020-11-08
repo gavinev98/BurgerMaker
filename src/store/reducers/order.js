f94c22d966a4938d58ffa66a173c900ee31abaf3
gavinev98 <gavin.everett48@hotmail.com>
2020-11-08 12:55:53 +0000

    created new order object to store data in array.

--- src/store/reducers/order.js
+++ src/store/reducers/order.js
@@ -16,18 +16,23 @@ const reducer = (state = initialState, action) => {
         switch (action.type) {
 
             case actionTypes.PURCHASE_BURGER_SUCCESS:
+                //create javascript object for each order. merging together.
                 const newOrder = {
                     ...action.orderData,
-                    id: action.
+                    id: action.id
                 }
                 return {
                     ...state,
                     loading: false,
-                    orders: state.orders.concat()
+                    orders: state.orders.concat(newOrder)
 
                 };
             case actionTypes.PURCHASE_BURGER_FAIL:
-                return {};
+                return {
+                    ...state,
+                    loading: false,
+                    
+                };
             default:
                 return state;
         }
