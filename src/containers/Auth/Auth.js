a588fa652cb2c11394a8c840fdbd4fd37b88e79a
gavinev98 <gavin.everett48@hotmail.com>
2020-11-12 14:26:03 +0000

    Setting up form controls for sign in.

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,13 +1,58 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+
+
 
 
 class Auth extends Component {
 
+    //this wont be handled in redux as we only care about local state.
+    state = {
+        controls : {
+            email: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'email',
+                    placeholder: 'Your email'
+                },
+                value: '',
+                validation: {
+                    required: true,
+                    isEmail: true
+                },
+                valid: false,
+                touched: false
+            },
+            pasword: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'password',
+                    placeholder: 'Password'
+                },
+                value: '',
+                validation: {
+                    required: true,
+                    minLength: 6
+                },
+                valid: false,
+                touched: false
+            }
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <form>
 
 
-    
+                </form>
+            </div>
 
 
+        );
+    }
 
 
 }
+
+export default Auth;
