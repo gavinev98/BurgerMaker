332ccb8d4a51c7b893a0692d34533bac3b787a68
gavinev98 <gavin.everett48@hotmail.com>
2020-11-14 15:34:54 +0000

    setting up axios auth request to firebase.

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,7 +1,7 @@
 
 import * as actionTypes from './actionTypes';
 //synchronous action creators for adding and removing items from burger.
-import axios from '../../axios-order';
+import axios from 'axios';
 
 
 export const authStart = () => {
@@ -29,12 +29,24 @@ export const authFailure = (error) => {
 };
 
 //asynch action creator which will be doing the auth stuff.
-export const auth = (email, pasword) => {
+export const auth = (email, password) => {
     return dispatch => {
         dispatch(authStart());
-        //post data to axios.
-        axios.post()
-
+        //post data to axios with api key, structure of data to be passed with request.
+        const authData = {
+            email : email,
+            password: password,
+            returnSecureToken: true
+        }
+        axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AAAA6mq6p0c:APA91bEzAWEVl8yya3hwCZwEpqQHX6SiukAUi2yvOFYHVbigFjjVpkRHJvqnmfvgXRNUiy3nra3g7cUCSlLg976LM9Z7k8mHXOOSaM8mphKBUz08hdFn-Pyu7A9yDz8n88cfPjoWy1QQ', authData)
+        .then(response => {
+            console.log(response);
+            dispatch(authSuccess(response.data));
+        })
+        .catch(err => {
+            console.log(err);
+            dispatch(authFailure(err))
+        });
         
 
     }
