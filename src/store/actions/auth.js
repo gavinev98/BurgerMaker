de5bcd38479f2ed8ad6b6b12723035a578f72437
gavinev98 <gavin.everett48@hotmail.com>
2020-11-16 15:22:06 +0000

    passing correct data for signin/login for tokens.

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -13,11 +13,12 @@ export const authStart = () => {
     };
 };
 
-export const authSuccess = (authData) => {
+export const authSuccess = (token, userId) => {
 
     return {
         type: actionTypes.AUTH_SUCCESS,
-        authData: authData
+        idToken: token,
+        userId: userId
     };
 };
 
@@ -48,7 +49,7 @@ export const auth = (email, password, isSignUp) => {
         axios.post(url, authData)
         .then(response => {
             console.log(response);
-            dispatch(authSuccess(response.data));
+            dispatch(authSuccess(response.data.idToken, response.data.localId));
         })
         .catch(err => {
             console.log(err);
