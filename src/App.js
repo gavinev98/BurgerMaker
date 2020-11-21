1690043b63878c23b113dea3bbe3e681fdd1861c
gavinev98 <gavin.everett48@hotmail.com>
2020-11-21 15:51:27 +0000

    creating routes for logout  case.

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import Layout from './components/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
 import Orders from './containers/Orders/Orders';
-
 import Auth from './containers/Auth/Auth';
+import Logout from './containers/Auth/Logout/Logout';
 
 class App extends Component {
   render() {
@@ -21,6 +21,7 @@ class App extends Component {
           <Route path="/checkout" component={Checkout} />
           <Route path="/orders" exact component={Orders} />
           <Route path="/auth" exact component={Auth} />
+          <Route path='/logout' exact component={Logout} />
           <Route path="/" exact component={BurgerBuilder} />
           </Switch>
         </Layout>
